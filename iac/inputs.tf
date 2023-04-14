1518e1a2399fadf8aa94f4f91fa2bc1bafd76735
huseyinbabal <huseyinbabal88@gmail.com>
2023-04-14 17:15:09 +0300

    tf cloud trigger

--- iac/inputs.tf
+++ iac/inputs.tf
@@ -1,13 +1,12 @@
-variable "project_id" {
-  type = string
-  default = "quizzer"
-}
-
 variable "region" {
   type = string
   default = "us-central1"
 }
 
 variable "SERVICE_ACCOUNT" {
+  type = string
+}
+
+variable "PROJECT_ID" {
   type = string
 }
